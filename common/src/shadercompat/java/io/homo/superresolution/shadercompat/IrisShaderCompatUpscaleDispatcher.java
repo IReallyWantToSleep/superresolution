00676dbfbd7e8829beb789a01bf307e6bb8583b0
eastear23333 <3792019371@qq.com>
2026-07-15 20:56:22 +0800

    修个bug

--- common/src/shadercompat/java/io/homo/superresolution/shadercompat/IrisShaderCompatUpscaleDispatcher.java
+++ common/src/shadercompat/java/io/homo/superresolution/shadercompat/IrisShaderCompatUpscaleDispatcher.java
@@ -420,28 +420,28 @@ public class IrisShaderCompatUpscaleDispatcher {
                 needsPreProcessDepth,
                 needsPreProcessMotionVectors
         );
+        if (SuperResolution.currentAlgorithm != null) {
+            SuperResolutionAPI.EVENT_BUS.post(
+                    new AlgorithmDispatchEvent(
+                            SuperResolution.currentAlgorithm,
+                            dispatchResource
+                    )
+            );
+        }
         if (!frameGenOnly) {
             GlDebug.pushGroup(64108436, "SR Upscale");
-            if (SuperResolution.currentAlgorithm != null) {
-                SuperResolutionAPI.EVENT_BUS.post(
-                        new AlgorithmDispatchEvent(
-                                SuperResolution.currentAlgorithm,
-                                dispatchResource
-                        )
-                );
-            }
             try (GlState ignored_ = new GlState()) {
                 SuperResolution.getCurrentAlgorithm().dispatch(dispatchResource);
             }
-            if (SuperResolution.currentAlgorithm != null) {
-                SuperResolutionAPI.EVENT_BUS.post(
-                        new AlgorithmDispatchFinishEvent(
-                                SuperResolution.currentAlgorithm,
-                                SuperResolution.currentAlgorithm.getOutputFrameBuffer()
-                        )
-                );
-            }
             GlDebug.popGroup();
+        }
+        if (SuperResolution.currentAlgorithm != null) {
+            SuperResolutionAPI.EVENT_BUS.post(
+                    new AlgorithmDispatchFinishEvent(
+                            SuperResolution.currentAlgorithm,
+                            SuperResolution.currentAlgorithm.getOutputFrameBuffer()
+                    )
+            );
         }
         /*
         升采样阶段结束
