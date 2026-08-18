681751f056bac6d55f3a37773a611e615c90975e
187J3X1-114514 <smjwoaini@qq.com>
2026-08-18 21:00:13 +0800

    remove unused shaders

--- common/src/main/resources/shader/interop/depth_preprocess.frag.glsl
+++ common/src/main/resources/shader/interop/depth_preprocess.frag.glsl
@@ -1,13 +1,16 @@
 #version 430 core
+
 precision highp float;
+
 layout(location = 0) in vec2 vTexCoord;
 layout(location = 0) uniform sampler2D inputDepth;
 layout(location = 0) out float fragColor;
+
 void main()
 {
     float depth = texture(
             inputDepth,
-            vec2(vTexCoord.x, 1.0 - vTexCoord.y) // 对于Vulkan纹理坐标系，需要翻转Y轴
+            vec2(vTexCoord.x, 1.0 - vTexCoord.y) // We need flip y axis for vulkan
         ).r;
     gl_FragDepth = depth;
     fragColor = depth;
